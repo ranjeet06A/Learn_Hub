b83ec043658cb07b90368b8ca739177f4f54238f
Ranjeet Kumar <ranjeet@example.com>
2026-05-01 17:47:23 +0530

    added frontend properly

--- src/components/AdminRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { isAdmin } from "../utils/auth";
-
-export default function AdminRoute({ children }: any) {
-  if (!isAdmin()) {
-    return <Navigate to="/login" />;
-  }
-  return children;
-}
